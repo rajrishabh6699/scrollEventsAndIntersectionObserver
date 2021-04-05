3bcd518fb0fca8810cb727fe4afce6f82cf9ccf1
Apple <apple@Apples-MacBook-Air.local>
2021-04-05 16:35:34 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,23 @@
+const terms = document.querySelector(".terms-and-conditions");
+const watch = document.querySelector(".watch");
+const button = document.querySelector(".accept");
+
+// terms.addEventListener("scroll", function(e){
+//     console.log(e.currentTarget.scrollTop);
+//     console.log(e.currentTarget.scrollHeight);
+// }
+// );
+
+function obCallback(payload){
+    if(payload[0].intersectionRatio===1){
+        button.disabled = false;
+        //stop observing the button
+        ob.unobserve(terms.lastElementChild);
+    }
+}
+
+const ob = new IntersectionObserver(obCallback,{
+    root:terms,
+    threshold:1,
+});
+ob.observe(terms.lastElementChild);
